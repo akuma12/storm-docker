c9a6cae94876154e298f553ab6d7de0563d6f19f
Jim Rohrer <jrohrer1@gmail.com>
2016-06-03 12:34:05 -0400

    Removed storm.local.hostname

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -2,6 +2,4 @@ export IP=`hostname -i`
 sed -i -e "s/%pacemaker%/$PM_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
-echo "\nstorm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-
 /usr/sbin/sshd && supervisord
--- storm-pacemaker/start-supervisor.sh
+++ storm-pacemaker/start-supervisor.sh
@@ -2,6 +2,4 @@ export IP=`hostname -i`
 sed -i -e "s/%pacemaker%/$IP/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 
-echo "\nstorm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-
 /usr/sbin/sshd && supervisord
