9a429166ba8a118ee538a4fb60234061b791551a
Jim Rohrer <jrohrer1@gmail.com>
2016-06-03 10:51:57 -0400

    Tweaking startup scripts

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -1,4 +1,5 @@
 export IP=`hostname -i`
+sed -i -e "s/%pacemaker%/$PM_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
--- storm-pacemaker/start-supervisor.sh
+++ storm-pacemaker/start-supervisor.sh
@@ -1,4 +1,5 @@
-sed -i -e "s/%pacemaker%/$PM_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+export IP=`hostname -i`
+sed -i -e "s/%pacemaker%/$IP/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 
 echo "\nstorm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
