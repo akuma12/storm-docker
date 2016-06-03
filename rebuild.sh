5be94e51da9d8fc35d547ea4b15f96f0bde39885
Jim Rohrer <jrohrer1@gmail.com>
2016-06-02 23:09:40 -0400

    Fixed some syntax errors

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="akuma12/base" base
+docker build -t="akuma12/storm" storm
+docker build -t="akuma12/storm-pacemaker" storm-pacemaker
+docker build -t="akuma12/storm-nimbus" storm-nimbus
+docker build -t="akuma12/storm-supervisor" storm-supervisor
+docker build -t="akuma12/storm-ui" storm-ui
