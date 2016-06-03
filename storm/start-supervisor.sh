f5bfe7c9fc5bea31f50d36e05f60eeff273d5452
Jim Rohrer <jrohrer1@gmail.com>
2016-06-03 14:51:54 -0400

    Tweaking storm.yaml to match 1.0.1 specs

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,6 +1,4 @@
 sed -i -e "s/%pacemaker%/$PM_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 
-echo "\nstorm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-
 /usr/sbin/sshd && supervisord
